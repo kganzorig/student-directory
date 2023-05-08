22d721655077e6df7ffaa1fdb6ffb3c488113da2
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-05-08 19:02:03 +0100

    Taking arguments from command line

--- directory.rb
+++ directory.rb
@@ -1,24 +1,9 @@
 @students = []
 
-def input_students 
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end 
-end
-
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end  
 
@@ -53,6 +38,21 @@ def process(selection)
   end
 end 
 
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name =STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end 
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "_____________"
@@ -78,8 +78,8 @@ def save_students
   file.close
 end 
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -87,5 +87,17 @@ def load_students
   file.close
 end 
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end 
 # nothing happens until we call the methods
+try_load_students
 interactive_menu
