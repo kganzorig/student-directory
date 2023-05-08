22915c4e68137b41e7b6a6f96823f49904269d75
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-05-08 13:35:48 +0100

    Add interactive_menu method and call method

--- directory.rb
+++ directory.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else 
+      puts "I don't know what you meant, try again"
+    end 
+  end
+end 
+
+
 def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -25,20 +51,14 @@ end
 def print(students)
   students.each_with_index do |student, index| 
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end 
-  puts "Here is a list of names less than 12 characters:"
-  students.each_with_index do |student, index| 
-      if student[:name].length < 12  
-        puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-      end 
-    end 
+  end
 end 
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
+students = interactive_menu
 # nothing happens until we call the methods
 print_header
 print(students)
